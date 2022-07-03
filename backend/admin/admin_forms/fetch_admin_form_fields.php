dfcc8718917f48aa6c00714741b716ad14d691ff
sahoochinmay <sahoochinmay54@gmail.com>
2022-07-03 16:52:24 +0530

    Add form integration completed

--- backend/admin/admin_forms/fetch_admin_form_fields.php
+++ backend/admin/admin_forms/fetch_admin_form_fields.php
@@ -12,7 +12,7 @@ $token = $_COOKIE["admin_jwt"];
 // checking is the user authorized 
 if(auth($token)){
 
-    $sql = "SELECT form_field_id as field_id, form_field_title as field_name, form_field_tag1 as field_tag, form_field_type as field_type FROM form_field_table";
+    $sql = "SELECT form_field_id as field_id, form_field_title as field_name, form_field_tag1 as field_tag, form_field_type as field_type ,form_field_required as required FROM form_field_table";
     $query = $con -> prepare($sql);
     $query->execute();
 
--- js/globalFunction.js
+++ js/globalFunction.js
@@ -134,3 +134,9 @@ function adminSignOut() {
     location.href = "index.html";
   }
 }
+
+function arrayMove(arr, fromIndex, toIndex) {
+  var element = arr[fromIndex];
+  arr.splice(fromIndex, 1);
+  arr.splice(toIndex, 0, element);
+}
