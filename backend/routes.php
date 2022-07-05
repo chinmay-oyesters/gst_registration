700266acff3bd5e6a621963b4f24fdf2cab93a03
OyeVed1 <vedant@oyesters.in>
2022-07-05 11:27:10 +0530

    Added fetch form and dependencies route for admin Updated - 3

--- backend/routes.php
+++ backend/routes.php
@@ -93,4 +93,4 @@ $router->endpoint('edit_admin_form', 'admin/admin_forms/edit_admin_form', ['POST
 $router->endpoint('approve_form', 'admin/admin_forms/approve_form', ['POST'], FALSE, ['form_id', 'user_id']);
 $router->endpoint('delete_form', 'admin/admin_forms/delete_form', ['POST'], FALSE, ['form_id', 'user_id']);
 $router->endpoint('fetch_full_form', 'admin/admin_forms/fetch_full_form', ['POST'], FALSE, ['form_id']);
-$router->endpoint('fetch_form_dependencies', 'admin/admin_forms/fetch_form_dependencies', ['POST'], FALSE, ['form_id', 'user_id']);
+$router->endpoint('fetch_form_dependencies', 'admin/admin_forms/fetch_form_dependencies', ['POST'], FALSE, ['form_id']);
