cced04e4f57241edcca985dce3d01065646beac6
krishnpal-oyesters <krishnpal@oyesters.in>
2022-07-06 18:34:28 +0530

    edit customer and send otp error fixed

--- backend/admin/auth/forgot_password/admin_send_otp.php
+++ backend/admin/auth/forgot_password/admin_send_otp.php
@@ -25,60 +25,70 @@ if($query->rowCount() === 0){
 		"msg" => "You are not registered with us"
 	];
 }else{
-	$otp = rand(100000, 999999);
-	$body = "Dear costumer,
 
-	Your One Time Password (OTP) for Forgot Password recovery on Betting Application is ". $otp . 
-	". Please note, this OTP is valid only for 15 minutes. Please do not share this One Time Password with anyone.";
+	$sql = "SELECT razorpay_key, razorpay_secret, sender_name, send_from, hostname, password, port FROM integrations_table";
+    $query = $con -> prepare($sql);
 
-	$sendfrom = "krishnpal@oyesters.in";
-	$sendername = "Krishnpal Parmar";
-	$sendto = $admin_email;
-	$subject = "One Time Password (OTP) for Forgot Password recovery on Betting Application";
-
-	//Sending  
-	$mail = new PHPMailer();
-	$mail->isSMTP();
-	$mail->Host = "mail.oyesters.in";
-	$mail->SMTPAuth = true;
-	$mail->Username = "krishnpal@oyesters.in";
-	$mail->Password = "Krishnpal@1234";
-	$mail->Port = 465;
-	$mail->SMTPSecure = 'ssl';
-
-	$mail->isHTML(true);
-	$mail->setFrom("$sendfrom", "$sendername");
-	$mail->addAddress($sendto);
-	$mail->Body="$body";
-	$mail->Subject="$subject";
-	if(!$mail->Send()) {
-		$status = 203;
-        $response = [
-            "msg" => "OTP not sent to mail"
-        ];				
+    if($query->execute()){
+		$integration_details = $query->fetchAll(PDO::FETCH_ASSOC)[0];
 		
-	} else {
-		$sql = "UPDATE admin_user_table SET 
-		otp = :otp,
-		otp_created_at = :otp_created_at,
-		updated_at = :updated_at
-		WHERE admin_email = :admin_email";
-		$query = $con->prepare($sql);
-		$query->bindParam(':otp', $otp, PDO::PARAM_STR);
-		$query->bindParam(':admin_email', $admin_email, PDO::PARAM_STR);
-		$query->bindParam(':otp_created_at', $datetime, PDO::PARAM_STR);
-		$query->bindParam(':updated_at', $datetime, PDO::PARAM_STR);
-		if($query->execute()){
-			$status = 200;
-			$response = [
-				"msg" => "OTP Successfully sent to :- "."$sendto"."<br>"
-			];			
-		}else{
+		$otp = rand(100000, 999999);
+		$body = "Dear costumer,
+	
+		Your One Time Password (OTP) for Forgot Password recovery on Betting Application is ". $otp . 
+		". Please note, this OTP is valid only for 15 minutes. Please do not share this One Time Password with anyone.";
+	
+		$sendfrom = $integration_details['send_from'];
+		$sendername = $integration_details['sender_name'];
+		$sendto = $admin_email;
+		$subject = "One Time Password (OTP) for Forgot Password recovery on Betting Application";
+	
+		//Sending  
+		$mail = new PHPMailer();
+		$mail->isSMTP();
+		$mail->Host = $integration_details['hostname'];
+		$mail->SMTPAuth = true;
+		$mail->Username = $integration_details['send_from'];
+		$mail->Password = $integration_details['password'];
+		$mail->Port = $integration_details['port'];
+		$mail->SMTPSecure = 'ssl';
+	
+		$mail->isHTML(true);
+		$mail->setFrom("$sendfrom", "$sendername");
+		$mail->addAddress($sendto);
+		$mail->Body="$body";
+		$mail->Subject="$subject";
+		if(!$mail->Send()) {
 			$status = 203;
 			$response = [
-				"msg" => "Internal Server Error"
-			];
+				"msg" => "OTP not sent to mail"
+			];				
+			
+		} else {
+			$sql = "UPDATE admin_user_table SET 
+			otp = :otp,
+			otp_created_at = :otp_created_at,
+			updated_at = :updated_at
+			WHERE admin_email = :admin_email";
+			$query = $con->prepare($sql);
+			$query->bindParam(':otp', $otp, PDO::PARAM_STR);
+			$query->bindParam(':admin_email', $admin_email, PDO::PARAM_STR);
+			$query->bindParam(':otp_created_at', $datetime, PDO::PARAM_STR);
+			$query->bindParam(':updated_at', $datetime, PDO::PARAM_STR);
+			if($query->execute()){
+				$status = 200;
+				$response = [
+					"msg" => "OTP Successfully sent to :- "."$sendto"."<br>"
+				];			
+			}else{
+				$status = 203;
+				$response = [
+					"msg" => "Internal Server Error"
+				];
+			}
 		}
+	}else{
+
 	}
 
 }
--- backend/admin/users/customers/edit_customers.php
+++ backend/admin/users/customers/edit_customers.php
@@ -12,35 +12,32 @@ if(auth($token)){
 
     //fetch details from query parameter
     $user_id = $_POST['user_id'];
-    $username = $_POST['username'];
-    $entity_name = $_POST['entity_name'];
+    $entity_fullname = $_POST['entity_fullname'];
     $entity_pan = $_POST['entity_pan'];
     $entity_phonenumber = $_POST['entity_phonenumber'];
     $entity_email = $_POST['entity_email'];
-    $person_name = $_POST['person_name'];
-    $person_email = $_POST['person_email'];
-    $person_phonenumber = $_POST['person_phonenumber'];
+    $user_fullname = $_POST['user_fullname'];
+    $user_email = $_POST['user_email'];
+    $user_phonenumber = $_POST['user_phonenumber'];
 
     //fetch details from market
     $sql = "UPDATE user_table SET 
-        username = :username,
-        entity_name = :entity_name,
+        entity_fullname = :entity_fullname,
         entity_pan = :entity_pan,
         entity_phonenumber=:entity_phonenumber,
         entity_email = :entity_email,
-        person_name = :person_name,
-        person_email = :person_email,
-        person_phonenumber = :person_phonenumber
+        user_fullname = :user_fullname,
+        user_email = :user_email,
+        user_phonenumber = :user_phonenumber
         WHERE user_id=:user_id";
     $query = $con -> prepare($sql);
-    $query->bindParam(':username', $username, PDO::PARAM_STR);
-    $query->bindParam(':entity_name', $entity_name, PDO::PARAM_STR);
+    $query->bindParam(':entity_fullname', $entity_fullname, PDO::PARAM_STR);
     $query->bindParam(':entity_pan', $entity_pan, PDO::PARAM_STR);
     $query->bindParam(':entity_phonenumber', $entity_phonenumber, PDO::PARAM_STR);
     $query->bindParam(':entity_email', $entity_email, PDO::PARAM_STR);
-    $query->bindParam(':person_name', $person_name, PDO::PARAM_STR);
-    $query->bindParam(':person_email', $person_email, PDO::PARAM_STR);
-    $query->bindParam(':person_phonenumber', $person_phonenumber, PDO::PARAM_STR);
+    $query->bindParam(':user_fullname', $user_fullname, PDO::PARAM_STR);
+    $query->bindParam(':user_email', $user_email, PDO::PARAM_STR);
+    $query->bindParam(':user_phonenumber', $user_phonenumber, PDO::PARAM_STR);
     $query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
     if($query->execute()){
         $status = 200;
