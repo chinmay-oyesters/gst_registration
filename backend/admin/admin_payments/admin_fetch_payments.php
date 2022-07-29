82ce057b34402333cbc35df10043e363ee785d27
krishnpal-oyesters <krishnpal@oyesters.in>
2022-07-29 20:44:01 +0530

    fetch payment in desc order modification done

--- backend/admin/admin_payments/admin_fetch_payments.php
+++ backend/admin/admin_payments/admin_fetch_payments.php
@@ -10,12 +10,13 @@ $token = $_COOKIE["admin_jwt"];
 // checking is the user authorized 
 if(auth($token)){
     
-    $sql = "SELECT ut.user_id, ut.user_fullname, ut.entity_fullname, ut.user_email, ut.user_phonenumber, 
-    pt.order_id, pt.razorpay_payment_id, pt.order_amount, ft.form_name FROM user_table as ut
-    LEFT JOIN payments_table as pt
+    $sql = "SELECT pt.payment_id, ut.user_id, ut.user_fullname, ut.entity_fullname, ut.user_email, ut.user_phonenumber, 
+    pt.order_id, pt.razorpay_payment_id, pt.order_amount, ft.form_name FROM payments_table as pt
+    LEFT JOIN user_table as ut
     ON ut.user_id = pt.user_id
     LEFT JOIN form_table as ft
-    ON pt.form_id = ft.form_id";
+    ON pt.form_id = ft.form_id
+    ORDER BY pt.payment_id desc";
     $query = $con -> prepare($sql);
 
     $query->execute();
